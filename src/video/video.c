c94db9471645905120be2be961d0ace376663f4e
siers <wimuan@gmail.com>
2012-06-03 02:17:54 +0300

    Video interface init.

--- src/video/video.c
+++ src/video/video.c
@@ -1,9 +1,36 @@
 #include "everything.h"
+#include "video/video.h"
+
+#ifdef __linux
+#include "video/linux.h"
+#endif /* __linux */
+
+/* the sane defaults */
+int video_width  = 640;
+int video_height = 480;
 
 int
 video_init()
 {
     log("initializing video");
+    video_specific_init();
 
     return 0;
 }
+
+int
+video_get_array_size()
+{
+    return width * height * 2;
+}
+
+char*** matrix
+video_get_array()
+{
+    return video_get_array();
+}
+
+/* FOOTNOTE: The matrix is 2-bytes for each cell, so
+ * it's size is width * height * 2.
+ * 2 bytes -- Y' & UV, respectively. Y' = luma, UV = chrominance.
+ */
--- src/video/video.h
+++ src/video/video.h
@@ -2,5 +2,12 @@
 #define VIDEO_H
 
 int video_init();
+int video_die();
+
+struct video {
+    int width = 640;
+    int height = 480;
+    int fd;
+} video;
 
 #endif /* VIDEO_H */
