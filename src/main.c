c04a065c29e58daf5d572153449d0a281769c94d
siers <wimuan@gmail.com>
2012-06-10 14:55:09 +0300

    Getting ready to be pushed.

--- src/main.c
+++ src/main.c
@@ -1,5 +1,5 @@
 /*  Voculus -- the eye of webcam.
-    Copyright (C) 2011 siers
+    Copyright (C) 2012 siers
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
